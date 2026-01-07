807a5aa3a8938ff1403ddd4dd63368eb95f553de
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2026-01-07 11:16:02 -0500

    add bootstrap icons

--- rctreportviewer/html/write_model_html.py
+++ rctreportviewer/html/write_model_html.py
@@ -25,6 +25,7 @@ def write_html_file(rct_detailed_report):
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
